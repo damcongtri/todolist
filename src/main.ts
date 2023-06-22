36631e973d7fe72c1faf1508436a42b7420914d9
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2023-06-23 01:31:12 +0700

    fix cors2

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe)
-  app.enableCors({
-    origin: 'https://todolist-fe-nu.vercel.app'
-  });
+  app.useGlobalPipes(new ValidationPipe())
+  app.use(cors());
   await app.listen(5678);
 }
 bootstrap();
