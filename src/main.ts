8abc5d694e401b4cf828b1f9842a3ae38d3a620f
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2023-06-23 01:51:55 +0700

    fix cors6

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe)
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   allowedHeaders: '*',
+  //   origin: '*',
+  //   credentials: true,
+  // });
   await app.listen(5678);
 }
 bootstrap();
